4716efd0b00ce381578ce433ce169cb39de7d43d
Ale Franzoni Gimenez <ale.dgk@gmail.com>
2023-10-15 12:46:46 -0300

    added execution arguments for scroll delay/retries and login credentials

--- inscraper.py
+++ inscraper.py
@@ -1,24 +1,38 @@
 from modules.scraper import Scraper
+from modules.argument_manager import ArgumentManager, ArgumentOptions
 from modules import stats
 from modules import file_io
+from getpass import getpass
 
 username = "<your_username>"
 password = "<your_password>"
 
 def main():
-    scraper = Scraper()
+    global username
+    global password
+    args = ArgumentManager()
+    scraper = Scraper(arguments=args)
+
+    # Login
+    if args.get(ArgumentOptions.ASK_LOGIN):
+        print("🔐 Account credentials is required")
+        username = input("🔑 Enter username: ")
+        print("🔑 ", end="", flush=True)
+        password = getpass("Enter password: ")
 
     if not scraper.authenticate(username, password):
         exit()
+
+    # Get data
     users = scraper.get_users()
     scraper.close()
 
     print("🔎 Almost done! Doing some maths...")
 
-    #Stats
+    # Stats
     diff = stats.diff(users)
 
-    #Save results
+    # Save results
     file_io.save(username, users, diff)
 
     print("\n✅ Done, thanks for using Inscraper!")
--- modules/scraper.py
+++ modules/scraper.py
@@ -1,28 +1,38 @@
 import re, time
 from playwright.sync_api import sync_playwright, TimeoutError as PlaywrightTimeoutError
+from modules.argument_manager import ArgumentManager, ArgumentOptions
 
 BASE_URL = "https://www.instagram.com/"
 FOLLOWERS_URL = BASE_URL + "{}/followers/"
 FOLLOWING_URL = BASE_URL + "{}/following/"
 
 class Scraper(object):
-    """Playwright based Instagram scraper. 
-    It's able to start up a browser, authenticate to Instagram and get the followers/following list from a specific user given."""
+    """
+    Playwright based Instagram scraper. 
+    It's able to start up a browser, authenticate to Instagram and get the followers/following list from a specific user given.
+    """
     
-    def __init__(self, headless=True):
+    def __init__(self, headless=True, arguments:ArgumentManager=None):
         """Initialize the Scraper object."""
         
         print("🚀 The environment is getting ready...")
         self.headless = headless
+        self.args = arguments
+        self.scroll_retries = self._get_scroll_retries()
+        self.scroll_delay = self._get_scroll_delay()
         self.browser = sync_playwright().start()
 
     def close(self):
-        """Close the browser."""
+        """
+        Close the browser.
+        """
 
         self.browser.close()   
         
     def authenticate(self, username, password):
-        """Authenticate to Instagram with the provided credentials."""
+        """
+        Authenticate to Instagram with the provided credentials.
+        """
 
         print("🌎 Navigating to Instagram page")
 
@@ -49,12 +59,14 @@ class Scraper(object):
             print("🔓 Successful login")
             return True
         except PlaywrightTimeoutError:
-            print("🧐 Oops! Seems it's taking more than usual to load the login page. Try again later")
+            print("🧐 Oops! Seems it's taking more than usual to load the login page. Check your credentials or connection and try again later")
             self.close()
             return False
         
     def get_users(self):
-        """Get the list of followers and following users in that order."""
+        """
+        Get the list of followers and following users in that order.
+        """
         
         groups = ["followers", "following"]
         
@@ -69,7 +81,9 @@ class Scraper(object):
         return followers, following
 
     def _check_protection_auth(self):
-        """Check if two factor authentication is required."""
+        """
+        Check if two factor authentication is required.
+        """
 
         try:
             self.page.wait_for_function('() => window.location.href.includes("login/two_factor")', timeout=5000)
@@ -78,14 +92,18 @@ class Scraper(object):
             return False
         
     def _open_modal(self, link):
-        """Open the modal with the list of followers or following users."""
+        """
+        Open the modal with the list of followers or following users.
+        """
 
         self.page.goto(link, wait_until="domcontentloaded")
         self.page.wait_for_selector("div[class='_aano']")
         time.sleep(1)
 
     def _get_link(self, group):
-        """Get the link to the followers or following list."""
+        """
+        Get the link to the followers or following list.
+        """
 
         print("👥 Opening {} list".format(group))
 
@@ -97,15 +115,19 @@ class Scraper(object):
             return None
         
     def _get_users_loaded(self):
-        """Get the number of users loaded in the modal."""
+        """
+        Get the number of users loaded in the modal.
+        """
 
         return self.page.evaluate('document.querySelector(\'div[class="_aano"]\').childNodes.item(0).childNodes.item(0).childElementCount')
 
     def _scroll_to_bottom(self, list):
-        """Scroll the modal to the bottom."""
+        """
+        Scroll the modal to the bottom.
+        """
 
         scroll_tries = 0
-        while scroll_tries < 6:
+        while scroll_tries < self.scroll_retries:
             users_loaded = self._get_users_loaded()
             self._scroll()
             new_users_loaded = self._get_users_loaded()
@@ -119,14 +141,38 @@ class Scraper(object):
         
         print(f"\r⏳ Wait, getting {list}... {new_users_loaded} users loaded")
     
+    def _get_scroll_retries(self, default=6):
+        """
+        Get the number of retries for scrolling the modal. The default number of retries is 6.
+        If the SCROLL_RETRIES argument is provided, it will be added to the default number of retries.
+        """
+
+        if not self.args.get(ArgumentOptions.SCROLL_RETRIES) == None:
+            default += self.args.get(ArgumentOptions.SCROLL_RETRIES)
+        return default
+
     def _scroll(self):
-        """Scroll the modal by setting the scroll-top position to max."""
+        """
+        Scroll the modal by setting the scroll-top position to max.
+        """
 
         self.page.evaluate('document.querySelector(\'div[class="_aano"]\').scrollTop = document.querySelector(\'div[class="_aano"]\').scrollTopMax')
-        time.sleep(.5)
+        time.sleep(self.scroll_delay)
+
+    def _get_scroll_delay(self, default=0.5):
+        """
+        Get the delay time for scrolling the modal. The default delay time is 0.5 seconds.
+        If the SCROLL_DELAY argument is provided, it will be added to the default delay time.
+        """
+
+        if not self.args.get(ArgumentOptions.SCROLL_DELAY) == None:
+            default += self.args.get(ArgumentOptions.SCROLL_DELAY)
+        return default
 
     def _get_list(self):
-        """Get the list of followers or following users from HTML content."""
+        """
+        Get the list of followers or following users from HTML content.
+        """
 
         pattern = r'href="/([^/"]*?)/" role="link"'
         html_content = self.page.content()
